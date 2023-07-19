b90135531f845d8f6eee4861125dd5a39f57e81c
bre97-web <by.sgatn3@gmail.com>
2023-07-19 20:13:04 +0800

    run

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -4,11 +4,17 @@
             <h1 class="text-2xl mb-4">Recent Played</h1>
             <RecentGames></RecentGames>
         </div>
+
+        <div class="">
+            <h1 class="text-2xl mb-4">Library</h1>
+            <OwnedGames></OwnedGames>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import RecentGames from './RecentGames.vue';
+import OwnedGames from './OwnedGames.vue';
 
 
 </script>
--- src/components/RecentGames.vue
+++ src/components/RecentGames.vue
@@ -1,8 +1,12 @@
 <template>
     <ul v-if="recentGames.getRecentGames !== null" class="flex gap-2 ">
         <li v-for="e in recentGames.getRecentGames" :key="e.appid">
-            <Card :game="e"></Card>
-            <div class=""></div>
+            <!-- <Card :game="e"></Card> -->
+            <div class="window relative rounded-xl max-w-xs overflow-clip">
+                <img class="scaleWindow" :src="useSteamMediaUrl(e.appid, MediaJpgType.header)" />
+                <h1 class="innerWindow absolute w-full h-full bg-gradient-to-t from-black to-transparent"></h1>
+                <h1 class="innerWindow absolute w-full text-center invert dark:invert-0">{{  }}</h1>
+            </div>
         </li>
     </ul>
 </template>
@@ -11,6 +15,7 @@
 import { SteamGame, useSteamMediaUrl, MediaJpgType } from '@/hooks/useSteam';
 import { onMounted } from 'vue';
 import { useGameStore } from '@/store/useGameStore'
+import moment from 'moment';
 
 const recentGames = useGameStore()
 
@@ -23,7 +28,7 @@ const Card = ({game}: {game: SteamGame}) => {
         <div class="window relative rounded-xl max-w-xs overflow-clip">
             <img class="scaleWindow" src={useSteamMediaUrl(game.appid, MediaJpgType.header)}></img>
             <h1 class="innerWindow absolute w-full h-full bg-gradient-to-t from-black to-transparent"></h1>
-            <h1 class="innerWindow absolute w-full text-center invert dark:invert-0">{ game.name }</h1>
+            <h1 class="innerWindow absolute w-full text-center invert dark:invert-0">{ moment(game.playtime_windows_forever, 'X').hours() }</h1>
         </div>
     )
 }
